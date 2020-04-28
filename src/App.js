bfe4b3a077b952a6910d6f6a130ffc91b768b5c7
Ashly Lau <ashlylau@gmail.com>
2020-04-28 17:54:34 +0800

    install tfjs and body-pix model, create webcam to take screenshot

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
 import React from 'react';
-import logo from './logo.svg';
+import Webcam from "react-webcam";
+
 import './App.css';
+
+const videoConstraints = {
+  width: 1280,
+  height: 720,
+  facingMode: "user"
+};
+
+
+class WebcamCapture2 extends React.Component {
+
+  constructor(props) {
+    super(props);    
+    this.webcamRef = React.createRef(null);
+    this.state = {
+      screenshot: null,
+    };
+
+    this.capture = this.capture.bind(this);
+  }
+
+  capture() {
+    const imageSrc = this.webcamRef.current.getScreenshot();
+    this.setState({screenshot: imageSrc});
+    console.log(imageSrc);
+  }
+
+  render() {
+    return (
+      <>
+      {!this.state.screenshot ? 
+      <Webcam
+          audio={false}
+          height={720}
+          ref={this.webcamRef}
+          screenshotFormat="image/jpeg"
+          width={1280}
+          videoConstraints={videoConstraints}
+          /> : <img src={this.state.screenshot}/>}
+        
+        <button onClick={this.capture}>Capture photo</button>
+      </>
+    );
+  }
+}
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <WebcamCapture2 />
+        
       </header>
     </div>
   );
